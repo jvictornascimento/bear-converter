6868d123a57a6dc203decb2c43f6bc426a02759a
jvictornascimento <victojoao789@gmail.com>
2026-04-26 16:44:12 -0300

    chore(setup): init project structure

---
+++ src/main/java/br/com/bearflow/bear_converter/BearConverterApplication.java
@@ -0,0 +1,13 @@
+package br.com.bearflow.bear_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bearflow/bear_converter/BearConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bearflow.bear_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
